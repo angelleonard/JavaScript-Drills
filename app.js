e13a7db91414721ebe14b087f0ae9e9e25076122
angelleonard <aleonard0215@aol.com>
2019-01-04 10:05:53 -0600

    assignment changes

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 let name = 'Angel';
-if (name[0] )
-if (name.charCodeAt >= 77) {
-    console.log ('Back of the line!')
-} else {
-    console.log('Next!')
-}
+
+
+
 const stateNumber = 50;
 var val = 5+4;
 console.log ('Hello World!')
