d844ecf9de6765c48aec40c63c4b61b0420ad019
angelleonard <aleonard0215@aol.com>
2019-01-04 10:04:36 -0600

    Change to assignment instructions

---
+++ app.js
@@ -0,0 +1,11 @@
+let name = 'Angel';
+if (name[0] )
+if (name.charCodeAt >= 77) {
+    console.log ('Back of the line!')
+} else {
+    console.log('Next!')
+}
+const stateNumber = 50;
+var val = 5+4;
+console.log ('Hello World!')
+let vegetables = [broccolli, squash, corn, green-beans]
