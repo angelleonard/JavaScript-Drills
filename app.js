bc875661d04292567e4be5e0fd7fe4c58e9bdbf8
angelleonard <aleonard0215@aol.com>
2019-01-11 16:39:40 -0600

    changes

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
 let name = 'Angel';
-
-
-
 const stateNumber = 50;
-var val = 5+4;
-console.log ('Hello World!')
-let vegetables = [broccolli, squash, corn, green-beans]
+var val = 5 + 4;
+function sayHello() {
+  alert('Hello World!')
+}
+sayHello()
+
+function checkAge(age, name) {
+  If(age < 21); {
+    alert("Sorry, " + name + " you aren't old enough to view this page!")
+  }
+
+  let people = [
+
+    {
+      name: 'Charles',
+      age: 21
+    },
+    {
+      name: 'Abby',
+      age: 27
+    },
+    {
+      name: 'James',
+      age: 18
+    },
+    {
+      name: 'John',
+      age: 17
+    }
+  ];
